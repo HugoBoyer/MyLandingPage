9d076e97ca5d6067c1f44f093bb3f210f02df093
Hugo <boyer.hugo974@gmail.com>
2025-10-30 23:57:22 +0100

    ajout de tailwind au projet

--- my-landing-page-app/src/App.jsx
+++ my-landing-page-app/src/App.jsx
@@ -1,9 +1,10 @@
-import './styles/global.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       
     </>
   )
---
+++ my-landing-page-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <>
+            <nav>
+            </nav>
+            <div>
+                <h1 className="text-red-500">Tailwind 4.1 fonctionne !</h1>
+            </div>
+        </>
+    )
+}
