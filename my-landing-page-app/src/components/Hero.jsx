512024e409ef69220e6359c4d8829924ee7d1142
Hugo <boyer.hugo974@gmail.com>
2025-11-03 22:54:07 +0100

    installation de react-router

--- my-landing-page-app/src/components/Hero.jsx
+++ my-landing-page-app/src/components/Hero.jsx
@@ -5,12 +5,14 @@ export default function Hero() {
         <section className="pl-35 pr-35 pt-40">
             <div className='flex'>
                 <div className='w-1/2'>
-                    <span className="uppercase text-[#7f7f7fff]">- Free 30 days trials</span>
-                    <p className='w-[490px]'>
-                        <h1 className="text-xl text-[72px] ">The best app conversion money</h1>
+                    <span className="uppercase text-[#7f7f7fff]">- Gratuit pendant 30 jours</span>
+                    <p>
+                        <h1 className="text-xl text-[72px] pb-5 leading-[80px]">Convertissez vos devises en un clic</h1>
                     </p>    
-                    <p>Here you can put a short description about your project</p>
-                    <button className="bg-[#3164F4] rounded-md px-8 py-6 text-sm text-white">Get strated</button>    
+                    <p>Rapide, simple et précis pour toutes vos transactions internationales.</p>
+                    <div className=' text-center'>
+                        <button className="bg-[#3164F4] rounded-md px-4 py-4 text-sm text-white mt-15">Essayer gratuitement</button>    
+                    </div>
                 </div>
                 <div  className='w-1/2 '>
                     <img src='https://assets.api.uizard.io/api/cdn/stream/b733eebc-b165-4038-af41-cdbd51d7aedd.png' />
@@ -20,11 +22,11 @@ export default function Hero() {
             <div className='align-center text-center'>
                 <span className='width-[40px]'>Trusted by individuals and team at the world's best companies</span>
                <div>
-                    <span className='text-[30px]'>Logo</span>
-                    <span className='text-[30px]'>Logo</span>
-                    <span className='text-[30px]'>Logo</span>
-                    <span className='text-[30px]'>Logo</span>
-                    <span className='text-[30px]'>Logo</span>            
+                    <span className='text-[30px] text-[#7f7f7fff] pr-10'>Logo</span>
+                    <span className='text-[30px] text-[#7f7f7fff] pr-10'>Logo</span>
+                    <span className='text-[30px] text-[#7f7f7fff] pr-10'>Logo</span>
+                    <span className='text-[30px] text-[#7f7f7fff] pr-10'>Logo</span>
+                    <span className='text-[30px] text-[#7f7f7fff] pr-10'>Logo</span>            
                </div>
             </div> 
         </section>
