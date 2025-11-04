6a42f6ef4b7e2388cc304a007b82827f98e47d80
Hugo <boyer.hugo974@gmail.com>
2025-11-04 02:00:32 +0100

    feat CSS amelioration de l'ui du systeme de conversion

--- my-landing-page-app/src/components/ConversionDevise.jsx
+++ my-landing-page-app/src/components/ConversionDevise.jsx
@@ -6,7 +6,7 @@ export default function ConversionDevise() {
     const [toDevise, setToDevise] = useState("EUR")
     const [montant, setMontant] = useState("")
     const [resultat, setResultat]  = useState(undefined)
-
+    
         const convertir = async () => {
             try {
                 const response = await axios.get(`https://api.frankfurter.app/latest?from=EUR`, {
@@ -26,31 +26,37 @@ export default function ConversionDevise() {
     
 
   return (
-    <div className="align-center text-center pt-40" style={{ padding: '20px', maxWidth: '400px', margin: 'auto' }}>
+    <div className=" pt-40 bg-white rounded-lg" style={{ padding: '20px', maxWidth: '400px', margin: 'auto' }}>
         <h1>Conversion de devise</h1>
-            <label>Montant :</label>
-            <input type="number"  value={montant || ""} onChange={(e) => setMontant(e.target.value)} />
-            <label>
-                Depuis :
-            </label>
-            <select value={fromDevise} onChange={(e) => setFromDevise(e.target.value)}>
-                <option value="USD">Dollar Americain (USD)</option>
-                <option value="EUR">Euro (EUR)</option>
-                <option value="GBP">livre Sterning (GBP)</option>
-                <option value="JPY">Yen Japonais (JPY)</option>
-            </select>
-            <label>
-                Vers :
-            </label>
-            <select value={toDevise} onChange={(e) => setToDevise(e.target.value)}>
-                <option value="USD">Dollar Americain (USD)</option>
-                <option value="EUR">Euro (EUR)</option>
-                <option value="GBP">livre Sterning (GBP)</option>
-                <option value="JPY">Yen Japonais (JPY)</option>
-            </select>
-            <button onClick={convertir}>Convertir</button>
-            <div>
-                {resultat !== undefined && <p>Résultat : {resultat.toFixed(2)}</p>}
+            <div className="inline w-full">
+                <label className="block ">Montant :</label>
+                <input className="border-[#E5E7EB] border-2 border-solid rounded-lg  p-3 w-full" type="number"  value={montant || ""} onChange={(e) => setMontant(e.target.value)} />
+            </div>
+            <div className="inline m-20">
+                <label className="block">
+                    Depuis :
+                </label>
+                <select className="border-[#E5E7EB] rounded-lg border-2 border-solid p-3 w-full" value={fromDevise} onChange={(e) => setFromDevise(e.target.value)}>
+                    <option value="USD">🇺🇸 Dollar Americain (USD)</option>
+                    <option value="EUR">🇪🇺 Euro (EUR)</option>
+                    <option value="GBP">🇬🇧 livre Sterning (GBP)</option>
+                    <option value="JPY">🇯🇵 Yen Japonais (JPY)</option>
+                </select>
+            </div>
+            <div className="inline m-20">
+                <label className="block">
+                    Vers :
+                </label>
+                <select className="border-[#E5E7EB] rounded-lg border-2 border-solid p-3 w-full" value={toDevise} onChange={(e) => setToDevise(e.target.value)}>
+                    <option value="USD">🇺🇸 Dollar Americain (USD)</option>
+                    <option value="EUR">🇪🇺 Euro (EUR)</option>
+                    <option value="GBP">🇬🇧 livre Sterning (GBP)</option>
+                    <option value="JPY">🇯🇵 Yen Japonais (JPY)</option>
+                </select>
+            </div>
+            <button className="mt-10 mb-10 p-5 block bg-[#E5E7EB] w-full" onClick={convertir}>Convertir</button>
+            <div className="bg-[#7B4DEE] h-40 align-item text-white rounded-lg text-center">
+                {resultat !== undefined && <p>Résultat : <p className="text-[40px]">{resultat.toFixed(2)}</p></p>}
             </div>
     </div>
   )
