90af4559d2c472c1d66f811cd04aa8ea483a1eea
Hugo <boyer.hugo974@gmail.com>
2025-11-06 22:41:17 +0100

    feat css stylisation du components introductionFeatures

--- my-landing-page-app/src/components/Hero.jsx
+++ my-landing-page-app/src/components/Hero.jsx
@@ -12,12 +12,12 @@ export default function Hero() {
                 <div className='flex'>
                     <div className='w-1/2'>
                         <span className="uppercase text-[#7f7f7fff]">- Gratuit pendant 30 jours</span>
-                        <h1 className="text-xl text-[72px] pb-5 leading-[80px]">Convertissez vos devises en un clic</h1>
+                        <h1 className="text-xl text-[72px] pb-5 leading-[80px]">Boost your productivity. Start using our app today.</h1>
                         <p>Rapide, simple et précis pour toutes vos transactions internationales.</p>
                         <div className=' text-center'>
                             <button
                                 onClick={() => navigate('/conversion-devise')}
-                                className="bg-[#3164F4] rounded-md px-4 py-4 text-sm text-white mt-15">
+                                className="bg-[#3164F4] rounded-md px-4 py-4 text-white mt-15">
                                 Essayer gratuitement
                             </button>    
                         </div>
